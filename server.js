b43033e7c5b7a5bd021b2de941efdc0f48dfafb2
KedyplasmTheDev <kedaar.patel@cois.edu.in>
2026-08-08 16:43:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,11 +7,12 @@ app.use(express.static(__dirname));
 
 let players = {};
 let kanyes = {};
+let portalPairs = {};
 let nextKanyeId = 1;
 
 let trumpState = {
-    x: -150,
-    z: -150,
+    x: -140,
+    z: -140,
     hp: 500,
     maxHp: 500,
     active: false
@@ -33,28 +34,45 @@ io.on('connection', (socket) => {
         id: socket.id,
         x: (Math.random() - 0.5) * 20,
         z: 25 + Math.random() * 10,
-        rotation: 0,
+        rotationY: 0,
+        rotationX: 0,
         nickname: "Goose",
         color: Math.random() * 0xffffff,
-        hp: 100
+        hp: 100,
+        selectedWeapon: "mic"
     };
 
     socket.emit('currentPlayers', players);
     socket.emit('currentKanyes', kanyes);
     socket.emit('trumpState', trumpState);
+    socket.emit('currentPortals', portalPairs);
     socket.broadcast.emit('newPlayer', players[socket.id]);
 
     socket.on('setNickname', (nickname) => {
         if (players[socket.id]) players[socket.id].nickname = nickname;
     });
 
-    socket.on('playerMovement', (movementData) => {
+    socket.on('playerMovement', (data) => {
         if (players[socket.id]) {
-            players[socket.id].x = movementData.x;
-            players[socket.id].z = movementData.z;
-            players[socket.id].rotation = movementData.rotation;
+            players[socket.id].x = data.x;
+            players[socket.id].z = data.z;
+            players[socket.id].rotationY = data.rotationY;
+            players[socket.id].rotationX = data.rotationX;
             socket.broadcast.emit('playerMoved', players[socket.id]);
         }
+    });
+
+    socket.on('switchWeapon', (weapon) => {
+        if (players[socket.id]) {
+            players[socket.id].selectedWeapon = weapon;
+            socket.broadcast.emit('playerSwitchedWeapon', { id: socket.id, weapon });
+        }
+    });
+
+    socket.on('firePortal', (data) => {
+        portalPairs[socket.id] = portalPairs[socket.id] || {};
+        portalPairs[socket.id][data.type] = { x: data.x, y: data.y, z: data.z };
+        io.emit('portalSpawned', { playerId: socket.id, type: data.type, pos: portalPairs[socket.id][data.type] });
     });
 
     socket.on('honk', () => {
@@ -96,6 +114,7 @@ io.on('connection', (socket) => {
 
     socket.on('disconnect', () => {
         delete players[socket.id];
+        delete portalPairs[socket.id];
         io.emit('playerDisconnected', socket.id);
     });
 });
